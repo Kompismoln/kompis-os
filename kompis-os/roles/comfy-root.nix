c8b7e65b3ae65b300b7957b6b2e364c5c7439a98
alex <alex@kompismoln.se>
2026-06-03 17:09:43 +0200

    new role: comfy-root

---
+++ kompis-os/roles/comfy-root.nix
@@ -0,0 +1,15 @@
+# kompis-os/roles/comfy-root.nix
+_: {
+
+  flake.nixosModules.comfy-root = {
+    imports = [
+      ../nixos/shell.nix
+    ];
+
+    config = {
+      kompis-os = {
+        shell.enable = true;
+      };
+    };
+  };
+}
